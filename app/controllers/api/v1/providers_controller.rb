b64b2c7e75725ca04cd40a2dcd266167a836ccd3
tmo macbook <trrl.mo@hotmail.com>
2021-08-05 14:46:30 -0400

    Modify 'providers::create' method to make external api call to request provider info with private class members.

--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -1,3 +1,5 @@
+require 'excon'
+
 module Api::V1
   class ProvidersController < ApplicationController
     def index
@@ -6,15 +8,41 @@ module Api::V1
     end
 
     def create
-      @provider = Provider.create(provider_params)
-      render json: @provider
+      puts provider_params
+      provider_response = request_provider(provider_params[:npi])
+      if provider_response
+        @provider = Provider.create(format_provider_response(provider_response))
+        render json: @provider
+      end
     end
 
     private
 
-      def provider_params
-        params.require(:provider)
-          .permit(:address, :name, :npi, :organization, :telephone_number)
-      end
+    def provider_params
+      params.require(:provider).permit(:npi)
+    end
+
+    def get_from_external_api(url)
+      response = Excon.get(url)
+      return nil if response.status != 200
+      JSON.parse(response.body)
+    end
+
+    def request_provider(npi)
+      get_from_external_api("https://npiregistry.cms.hhs.gov/api/?version=2.0&number=#{npi}")
+    end
+
+    def format_provider_response(provider_response)
+        result = provider_response.dig('results', 0)
+        provider_info = result['basic']
+        location = result['addresses'].find { |address| address['address_purpose'] == 'LOCATION'}
+        {
+          npi: result['number'],
+          name: "#{provider_info['name_prefix']} #{provider_info['first_name']} #{provider_info['last_name']} #{provider_info['credential']}",
+          telephone_number: location['telephone_number'],
+          address: "#{location['address_1']}, #{location['city']}, #{location['state']}, #{location['postal_code']}, #{location['country_name']}",
+          organization: result['enumeration_type'] == 'NPI-2'
+        }
+    end
   end
 end
